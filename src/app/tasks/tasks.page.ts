9b9fdf2a3ef768406eb437b42b71ac510c91b015
soares.lopes <soares.lopes@mail.uft.edu.br>
2023-08-07 16:38:56 -0300

    adicionando tasks page

---
+++ src/app/tasks/tasks.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.page.html',
+  styleUrls: ['./tasks.page.scss'],
+})
+
+export class TasksPage implements OnInit {
+
+  tasks: string[] = [];
+  newTask: string = '';
+  
+  addTask() {
+    if (this.newTask.trim() !== ''){
+      this.tasks.push(this.newTask);
+      this.newTask = '';
+    }
+  }
+
+  removeTask(index: number){
+    this.tasks.splice(index, 1);
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
